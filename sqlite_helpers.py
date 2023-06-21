4159e82acee085b272391e31799899a5f3770597
pnavab <114110926+pnavab@users.noreply.github.com>
2023-06-20 19:34:38 -0700

    Fix sqlite_helpers

--- sqlite_helpers.py
+++ sqlite_helpers.py
@@ -1,8 +1,4 @@
 import sqlite3
-import random
-import string
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import RedirectResponse, HTMLResponse
 from datetime import datetime, timedelta
 
 def maybe_create_table(sqlite_file: str) -> bool:
@@ -10,8 +6,8 @@ def maybe_create_table(sqlite_file: str) -> bool:
     cursor = db.cursor()
 
     try :
-        createTableQuery = "CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY, url TEXT, alias TEXT, timestamp DATETIME)"
-        cursor.execute(createTableQuery)
+        create_table_query = "CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY, url TEXT, alias TEXT UNIQUE, timestamp DATETIME)"
+        cursor.execute(create_table_query)
         db.commit()
         return True
     except Exception:
@@ -27,23 +23,8 @@ def insert_url(sqlite_file: str, url: str, alias: str):
         val = (url, alias, timestamp)
         cursor.execute(sql, val)
         db.commit()
-    except Exception:
-        return False
-
-def check_alias_exists(sqlite_file: str, alias: str): #returns True if the alias already exists
-    db = sqlite3.connect(sqlite_file)
-    cursor = db.cursor()
-    
-    try:
-        sql = "SELECT alias FROM urls WHERE alias = ?"
-        cursor.execute(sql, (alias,))
-        result = cursor.fetchone()
-
-        if result is not None:
-            return True
-        else:
-            return False
-    except Exception:
+        return True
+    except sqlite3.IntegrityError:
         return False
 
 def get_urls(sqlite_file: str): #returns all urls in the table
@@ -70,7 +51,6 @@ def get_urls(sqlite_file: str): #returns all urls in the table
 def get_url(sqlite_file: str, alias: str): #return the string for url entry for a specified alias
     db = sqlite3.connect(sqlite_file)
     cursor = db.cursor()
-    result = None
     
     try:
         sql = "SELECT * FROM urls WHERE alias = ?"
@@ -78,15 +58,21 @@ def get_url(sqlite_file: str, alias: str): #return the string for url entry for 
         result = cursor.fetchone()
 
         #delete the entry if it has been stored for over a year
-        yearAgo = datetime.now() - timedelta(days=365)
-        if result[3] < yearAgo:
-            sql = "DELETE FROM urls WHERE alias = ?"
-            val = alias
-            cursor.execute(sql, (val, ))
-            db.commit()
-            result = None  
-        return result[1]
-    except Exception:
+        if result:
+            year_ago_date = datetime.now() - timedelta(days=365)
+            result_datetime_str = result[3].split(".")[0]  # Remove fractional seconds
+            result_datetime = datetime.strptime(result_datetime_str, "%Y-%m-%d %H:%M:%S")
+            if result_datetime < year_ago_date:
+                sql = "DELETE FROM urls WHERE alias = ?"
+                cursor.execute(sql, (alias, ))
+                db.commit()
+                return None
+            else:
+                return result[1]
+        else:
+            return None
+    except Exception as e:
+        print("exception", e)
         return None
     
 def delete_url(sqlite_file: str, alias: str): #delete entry in the database from specified alias
