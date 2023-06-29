5021deb9a74acc5983073368ed08befa123185b5
pnavab <114110926+pnavab@users.noreply.github.com>
2023-06-29 00:33:55 -0700

    Add Enums for HTTP Response Code (#5)
    * Add sqlite_helpers
    
    * Fix sqlite_helpers
    
    * Add server.py
    
    * Delete requirements.txt
    
    * Clean up server.py
    
    * Fix server.py
    
    * Add enum constants for error codes
    
    * Delete server.py
    
    * Delete sqlite_helpers.py
    
    * Update constants.py
    
    * Update constants.py
    
    * Add enum constants for error codes
    
    * Add enum constants
    
    * Remove drop_table method

---
+++ constants.py
@@ -0,0 +1,20 @@
+import enum
+
+class HttpResponse(enum.Enum):
+  
+  OK = (200, "success")
+  BAD_REQUEST = (400, "<h1>No URL was found in your request</h1>")
+  NOT_FOUND = (404, "<h1>URL not found</h1>")
+  CONFLICT = (409, "<h1>Alias already exists</h1>")
+  INTERNAL_SERVER_ERROR = (500, "<h1>Internal server error</h1>")
+  def __init__(self, code, content):
+    self.code = code
+    self.content = content
+
+http_code_to_enum = {
+  200: HttpResponse.OK,
+  400: HttpResponse.BAD_REQUEST,
+  404: HttpResponse.NOT_FOUND,
+  409: HttpResponse.CONFLICT,
+  500: HttpResponse.INTERNAL_SERVER_ERROR,
+}
