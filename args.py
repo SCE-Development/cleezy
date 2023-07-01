91837b408d760397db1040ea07fed0c648c0c0be
pnavab <114110926+pnavab@users.noreply.github.com>
2023-06-30 21:44:27 -0700

    Add argparse.py

---
+++ args.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--database-file-path",
+        required=True,
+        help="path to sqlite database file"
+    )
+    parser.add_argument(
+        "--host",
+        default="0.0.0.0",
+        help="ip address for server to listen on, defaults to 0.0.0.0"
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8000,
+        help="port for server to be hosted on, defaults to 8000"
+    )
+    
+    return parser.parse_args()
