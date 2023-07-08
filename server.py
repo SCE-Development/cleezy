bea66ec48b19e71a27f28bde9fca4db23b7a6797
pnavab <114110926+pnavab@users.noreply.github.com>
2023-07-07 21:18:39 -0700

    Add Logging (#10)
    * Add logging to server.py and a flag in args.py to increase verbosity
    
    * Change logging messages to log inputted requests
    
    * Add KeyError exceptions to create_url endpoints
    
    * Update args.py
    
    * Remove logging statements
    
    ---------
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import RedirectResponse, HTMLResponse
+import logging
+import time
 import uvicorn
 
 from args import get_args
@@ -16,24 +18,28 @@ sqlite_helpers.maybe_create_table(DATABASE_FILE)
 
 @app.post("/create_url")
 async def create_url(request: Request):
-
     urljson = await request.json()
-    aliasVal = None
+    logging.debug(f"/create_url called with body: {urljson}")
+    alias = None
 
-    if "url" not in urljson:
-        raise HTTPException(status_code=HttpResponse.BAD_REQUEST.code)
-    elif args.disable_random_alias and "alias" not in urljson:
-        raise HTTPException(status_code=HttpResponse.BAD_REQUEST.code)
-    
-    if "alias" not in urljson:
-        aliasVal = generate_alias(urljson['url'])
-    else:
-        aliasVal = urljson['alias']
+    try:
+        alias = urljson.get('alias')
+        if alias is None:
+            if args.disable_random_alias:
+                raise KeyError("alias must be specified")
+            else:
+                alias = generate_alias()
 
-    if sqlite_helpers.insert_url(DATABASE_FILE, urljson['url'], aliasVal):
-        return { "alias": aliasVal }
-    else:
-        raise HTTPException(status_code=HttpResponse.CONFLICT.code )
+        if sqlite_helpers.insert_url(DATABASE_FILE, urljson['url'], alias):
+            return { "alias": alias }
+        else:
+            raise HTTPException(status_code=HttpResponse.CONFLICT.code )
+    except KeyError:
+        logging.exception("returning 400 due to missing key")
+        raise HTTPException(status_code=HttpResponse.BAD_REQUEST.code)
+    except Exception:
+        logging.exception("/create_url had an error")
+        raise HTTPException(status_code=HttpResponse.INTERNAL_SERVER_ERROR.code)
     
     
 @app.get("/list")
@@ -43,7 +49,7 @@ async def get_all_urls():
 
 @app.get("/find/{alias}")
 async def get_url(alias: str):
-
+    logging.debug(f"/find called with alias: {alias}")
     url_output = sqlite_helpers.get_url(DATABASE_FILE, alias)
     if url_output is None:
         raise HTTPException(status_code=HttpResponse.NOT_FOUND.code)
@@ -53,6 +59,7 @@ async def get_url(alias: str):
 
 @app.post("/delete/{alias}")
 async def delete_url(alias: str):
+    logging.debug(f"/delete called with alias: {alias}")
     if(sqlite_helpers.delete_url(DATABASE_FILE, alias)):
         return {"message": "URL deleted successfully"}
     else:
@@ -63,5 +70,15 @@ async def http_exception_handler(request, exc):
     status_code_enum = http_code_to_enum[exc.status_code]
     return HTMLResponse(content=status_code_enum.content, status_code=status_code_enum.code)
 
+logging.Formatter.converter = time.gmtime
+
+logging.basicConfig(
+    # in mondo we trust
+    format="%(asctime)s.%(msecs)03dZ %(levelname)s:%(name)s:%(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level= logging.ERROR - (args.verbose*10),
+)
+
 if __name__ == "__main__":
+    logging.info(f"running on {args.host}, listening on port {args.port}")
     uvicorn.run(app, host=args.host, port=args.port)
