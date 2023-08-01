d81b5fc8086c417bc237347ea303b0f803794b4e
pnavab <114110926+pnavab@users.noreply.github.com>
2023-07-31 17:04:51 -0700

    Fix Prometheus metrics

--- server.py
+++ server.py
@@ -27,20 +27,16 @@ url_count = prometheus_client.Counter(
     "Number of urls in the database",
 )
 
-sql_error_count = prometheus_client.Counter(
-    "sql_error_count",
-    "Number of sql errors that have occurred",
+http_error_codes = prometheus_client.Counter(
+    "http_error_codes",
+    "Number of HTTP errors labeled by code",
+    labelnames=["error_code"]
 )
 
-find_count = prometheus_client.Counter(
-    "find_count",
-    "Number of urls successfully found",
-)
-
-query_time = prometheus_client.Histogram(
+query_time = prometheus_client.Summary(
     "query_time",
     "Time taken to execute SQLite queries",
-    buckets=[0.1, 0.5, 1, 5],
+    labelnames=["query_type"]
 )
 
 #maybe create the table if it doesnt already exist
@@ -61,46 +57,46 @@ async def create_url(request: Request):
             else:
                 alias = generate_alias(urljson['url'])
 
-        if sqlite_helpers.insert_url(DATABASE_FILE, urljson['url'], alias):
-            url_count.inc(1)
-            return { "url": urljson['url'], "alias": alias }
-        else:
-            raise HTTPException(status_code=HttpResponse.CONFLICT.code )
+        with query_time.labels("create").time():
+            if sqlite_helpers.insert_url(DATABASE_FILE, urljson['url'], alias):
+                url_count.inc(1)
+                return { "url": urljson['url'], "alias": alias }
+            else:
+                raise HTTPException(status_code=HttpResponse.CONFLICT.code )
     except KeyError:
         logging.exception("returning 400 due to missing key")
         raise HTTPException(status_code=HttpResponse.BAD_REQUEST.code)
    
 @app.get("/list")
 async def get_all_urls():
-    return sqlite_helpers.get_urls(DATABASE_FILE)
+    with query_time.labels("list").time():
+      return sqlite_helpers.get_urls(DATABASE_FILE)
 
 
 @app.get("/find/{alias}")
 async def get_url(alias: str):
-    start_time = time.time()
     logging.debug(f"/find called with alias: {alias}")
-    url_output = sqlite_helpers.get_url(DATABASE_FILE, alias)
-    elapsed_time = time.time() - start_time
-    query_time.observe(elapsed_time)
+    with query_time.labels("find").time():
+        url_output = sqlite_helpers.get_url(DATABASE_FILE, alias)
 
     if url_output is None:
         raise HTTPException(status_code=HttpResponse.NOT_FOUND.code)
-    find_count.inc(1)
     return RedirectResponse(url_output)
     
 
 @app.post("/delete/{alias}")
 async def delete_url(alias: str):
     logging.debug(f"/delete called with alias: {alias}")
-    if(sqlite_helpers.delete_url(DATABASE_FILE, alias)):
-        return {"message": "URL deleted successfully"}
-    else:
-        raise HTTPException(status_code=HttpResponse.NOT_FOUND.code)
+    with query_time.labels("delete").time():
+      if(sqlite_helpers.delete_url(DATABASE_FILE, alias)):
+          return {"message": "URL deleted successfully"}
+      else:
+          raise HTTPException(status_code=HttpResponse.NOT_FOUND.code)
 
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
-    sql_error_count.inc(1)
     status_code_enum = http_code_to_enum[exc.status_code]
+    http_error_codes.labels(status_code_enum.code).inc(1)
     return HTMLResponse(content=status_code_enum.content, status_code=status_code_enum.code)
 
 @app.get("/metrics")
@@ -122,6 +118,5 @@ logging.basicConfig(
 if __name__ == "__main__":
     logging.info(f"running on {args.host}, listening on port {args.port}")
     initial_url_count = sqlite_helpers.get_number_of_entries(DATABASE_FILE)
-    logging.info(f"number of urls in the database is {initial_url_count}")
     url_count.inc(initial_url_count)
     uvicorn.run(app, host=args.host, port=args.port)
