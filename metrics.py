25853bc8aba7478cc67852c0059591c22dac374f
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2023-09-29 20:11:34 -0700

    Create Singleton class for Prometheus metrics (#20)

---
+++ metrics.py
@@ -0,0 +1,56 @@
+import enum
+
+import prometheus_client
+
+
+class Metrics(enum.Enum):
+    URL_COUNT = (
+        "url_count",
+        "Number of urls in the database",
+        prometheus_client.Counter,
+    )
+    HTTP_ERROR_CODES = (
+        "http_error_codes",
+        "Number of HTTP errors labeled by code",
+        prometheus_client.Counter,
+        ["error_code"],
+    )
+    QUERY_TIME = (
+        "query_time",
+        "Time taken to execute SQLite queries",
+        prometheus_client.Summary,
+        ["query_type"],
+    )
+
+    def __init__(self, title, description, prometheus_type, labels=()):
+        # we use the above default value for labels because it matches what's used
+        # in the prometheus_client library's metrics constructor, see
+        # https://github.com/prometheus/client_python/blob/fd4da6cde36a1c278070cf18b4b9f72956774b05/prometheus_client/metrics.py#L115
+        self.title = title
+        self.description = description
+        self.prometheus_type = prometheus_type
+        self.labels = labels
+
+
+class MetricsHandler:
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError('Call MetricsHandler.instance() instead')
+
+    def init(self) -> None:
+        for metric in Metrics:
+            setattr(
+                self,
+                metric.title,
+                metric.prometheus_type(
+                    metric.title, metric.description, labelnames=metric.labels
+                ),
+            )
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+            cls.init(cls)
+        return cls._instance
