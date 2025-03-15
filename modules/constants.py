183f5fcea946f847799d14128f38157fde09460d
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2025-03-15 13:18:02 -0700

    Revert "added more information to the not found page (#56)"
    This reverts commit f3aa0349262f549fd392272f40bb37b5b4d20229.

--- modules/constants.py
+++ modules/constants.py
@@ -1,18 +1,10 @@
 import enum
 
-NOT_FOUND_HTML = """
-<h1>Url not found.</h1>
-
-<p>The requested URL "{requested_url}" could not be found on this server.</p>
-
-<p>Return to <a href="{base_url}">homepage</a></p>
-"""
-
 class HttpResponse(enum.Enum):
   
   OK = (200, "success")
   BAD_REQUEST = (400, "<h1>No URL was found in your request</h1>")
-  NOT_FOUND = (404, NOT_FOUND_HTML)
+  NOT_FOUND = (404, "<h1>URL not found</h1>")
   CONFLICT = (409, "<h1>Alias already exists</h1>")
   INVALID_ARGUMENT_EXCEPTION = (422, "<h1>Alias is invalid</h1>")
   INTERNAL_SERVER_ERROR = (500, "<h1>Internal server error</h1>")
