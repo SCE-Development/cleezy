f3aa0349262f549fd392272f40bb37b5b4d20229
nathan tran <180703202+n8thantran@users.noreply.github.com>
2025-03-15 11:20:17 -0700

    added more information to the not found page (#56)
    * added more information to the not found page
    
    * fix the values

--- server.py
+++ server.py
@@ -175,8 +175,17 @@ async def qr(alias: str):
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
     status_code_enum = http_code_to_enum[exc.status_code]
+    if exc.status_code == HttpResponse.NOT_FOUND.code:
+        return HTMLResponse(
+            content=status_code_enum.content.format(
+                requested_url=str(request.url),
+                base_url=str(request.base_url)
+            ),
+            status_code=status_code_enum.code
+        )
     return HTMLResponse(
-        content=status_code_enum.content, status_code=status_code_enum.code
+        content=status_code_enum.content,
+        status_code=status_code_enum.code
     )
 
 
