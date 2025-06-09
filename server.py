db63bd0526df462ae28a71a09b41b11b0a8e75a8
kevintsoii <kevin.tsoi44@gmail.com>
2025-06-08 19:00:29 -0700

    test

--- server.py
+++ server.py
@@ -156,16 +156,19 @@ async def delete_url(alias: str):
 async def qr(alias: str):
     logging.debug(f"/qr code generation called with alias: {alias}")
     with MetricsHandler.query_time.labels("qr").time():
-
-        return '''
-        <html>
-  <head>
-    <meta property="og:image" content="https://sce.sjsu.edu/qr/discord.png" />
-  </head>
-  <body>
-    <img src="https://sce.sjsu.edu/qr/discord.png" alt="Discord QR Code" />
-  </body>
-</html>'''
+        return HTMLResponse(
+            content='''
+            <html>
+              <head>
+            <meta property="og:image" content="https://sce.sjsu.edu/qr/discord.png" />
+              </head>
+              <body>
+            <img src="https://sce.sjsu.edu/qr/discord.png" alt="Discord QR Code" />
+              </body>
+            </html>
+            ''',
+            media_type="text/html"
+        )
 
         maybe_image_data = qr_code_cache.find(alias)
         if maybe_image_data is not None:
