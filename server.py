76834039a44092beed3db1daf39c217e74641ca6
evan <evanuxd@gmail.com>
2026-08-20 20:40:01 -0700

    if exc.status_code not in http_code_to_enum:

--- server.py
+++ server.py
@@ -238,6 +238,10 @@ async def qr(alias: str):
 
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
+    if exc.status_code not in http_code_to_enum:
+        return HTMLResponse(
+            status_code=exc.status_code
+        )
     status_code_enum = http_code_to_enum[exc.status_code]
     content = status_code_enum.content
     if status_code_enum == HttpResponse.NOT_FOUND:
